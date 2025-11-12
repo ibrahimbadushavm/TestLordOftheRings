e647c2397703fcff77d754520ca1dd1db99932a2
ibrahimbadushavm <ibrahimbadu779@gmaail.com>
2025-11-12 18:29:53 +0530

    added dynamic test

---
+++ src/test/java/com/scaler/utils/ConverterUtilTest.java
@@ -0,0 +1,39 @@
+package com.scaler.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+class ConverterUtilTest {
+
+    int[][] celsiusFahrenheitMapping = new int[][] { { 10, 50 }, { 40, 104 }, { 0, 32 } };
+
+    @TestFactory
+    Stream<DynamicTest> ensureThatCelsiumConvertsToFahrenheit() {
+        return Arrays.stream(celsiusFahrenheitMapping).map(entry -> {
+            // access celcius and fahrenheit from entry
+            int celsius = entry[0];
+            int fahrenheit = entry[1];
+            // return a dynamicTest which checks that the conversion from celsius to
+            // fahrenheit is correct
+            return DynamicTest.dynamicTest("Testing of celsius "+celsius+" to fahrenheit "+fahrenheit,
+                    ()->assertEquals(fahrenheit,ConverterUtil.convertCelsiusToFahrenheit(celsius)));
+        });
+
+    }
+
+    @TestFactory
+    Stream<DynamicTest> ensureThatFahrenheitToCelsiumConverts() {
+        return Arrays.stream(celsiusFahrenheitMapping).map(entry->{
+            int celsius = entry[0];
+            int fahrenheit = entry[1];
+            return DynamicTest.dynamicTest("Testing of fahrenheit "+fahrenheit+" to celsius "+ celsius,
+                    ()->assertEquals(celsius,ConverterUtil.convertFahrenheitToCelsius(fahrenheit)));
+        });
+    }
+}
